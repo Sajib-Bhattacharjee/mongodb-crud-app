7d7db085ef9d16cf4a6c30f7b934dd872736ef82
Sajib Bhattacharjee <86997775+Sajib-Bhattacharjee@users.noreply.github.com>
2023-02-09 00:31:11 +0600

    updated indes.js file.

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 
 const PORT = 3002;
 
 const connectDB = require("./config/db");
-
-const userRouter = require("./routes/user.route");
-//mongoose.set("strictQuery", false);
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//All routes.
-app.use("/products", userRouter);
-// Create Product Schema.
-
-//Get Request From Home Route.
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-  res.status(200);
-});
 
 //Server Connection.
 app.listen(PORT, async () => {
